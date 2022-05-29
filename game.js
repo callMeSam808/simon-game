da1077cbb98e5d69e8620a9fe86a326a99f9f8dc
Oshamaru <callMeSam808@gmail.com>
2022-05-28 20:39:30 -1000

    Add sound effects to chosen colors.

--- game.js
+++ game.js
@@ -5,7 +5,31 @@ const buttonColours = ["red", "blue", "green", "yellow"];
 let randomChosenColour = buttonColours[nextSequence()];
 gamePattern.push(randomChosenColour);
 
-$("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+$("#"+randomChosenColour).fadeOut(100, playSound).fadeIn(100);
+
+
+
+function playSound() {
+  let audio;
+  switch (randomChosenColour) {
+    case "red":
+      audio = new Audio("sounds/red.mp3");
+      audio.play();
+      break;
+    case "blue":
+      audio = new Audio("sounds/blue.mp3");
+      audio.play();
+      break;
+    case "green":
+      audio = new Audio("sounds/green.mp3");
+      audio.play();
+      break;
+    case "yellow":
+      audio = new Audio("sounds/yellow.mp3");
+      audio.play();
+      break;
+  }
+}
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
